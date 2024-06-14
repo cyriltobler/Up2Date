5bc082a431cec3f0e2a210120184f40e9c8cfff1
Cyril Tobler <cyriltoibler@icloud.com>
2024-06-14 21:54:43 +0200

    #6: Add the channel image and domain to the API

--- api/src/getArticles.js
+++ api/src/getArticles.js
@@ -3,7 +3,34 @@ const getDB = require('./db');
 async function getArticles() {
     const db = await getDB();
     const collection = db.collection('article');
-    const output = await collection.find().limit(5).toArray();
+    // const output = await collection.find().sort({ isoDate: -1 }).toArray();
+    const output = await collection.aggregate([
+        {
+            $sort: { isoDate: -1 },
+        },
+        {
+            $lookup: {
+                from: 'channel',
+                localField: 'channelFK',
+                foreignField: '_id',
+                as: 'channelDetails',
+            },
+        },
+        {
+            $unwind: '$channelDetails',
+        },
+        {
+            $project: {
+                title: 1,
+                isoDate: 1,
+                link: 1,
+                img: 1,
+                domain: '$channelDetails.domain',
+                channelImg: '$channelDetails.channelImg',
+                _id: 0,
+            },
+        },
+    ]).toArray();
     return output;
 }
 
